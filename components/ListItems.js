845b62a247ed40767318222b3652f10a5670cf06
SaidNivek <kevin.vi.dias@gmail.com>
2022-09-08 16:41:23 -0400

    Add swipe-able property to to-do list items

--- components/ListItems.js
+++ components/ListItems.js
@@ -1,10 +1,54 @@
 import React from 'react'
 
-import { Text } from "react-native"
+// import { Text } from "react-native"
 
-function ListItems() {
+import { SwipeListView } from "react-native-swipe-list-view"
+import { Entypo } from "@expo/vector-icons"
+
+// styled components
+import {
+    ListView,
+    ListViewHidden,
+    TodoText,
+    TodoDate,
+    HiddenButton,
+    SwipedTodoText,
+    colors
+} from "../styles/appStyles"
+
+function ListItems({todos, setTodos}) {
   return (
-    <Text>List Items Go Here</Text>
+    <SwipeListView 
+        data={todos}
+        renderItem={(data) => {
+            return (
+                <ListView>
+                    <>
+                        <TodoText>{data.item.title}</TodoText>
+                        <TodoDate>{data.item.date}</TodoDate>
+                    </>
+                </ListView>
+            )
+        }}
+        renderHiddenItem={() => {
+            return (
+                <ListViewHidden>
+                    <HiddenButton>
+                        <Entypo name="trash" size={25} color={colors.secondary} />
+                    </HiddenButton>
+                </ListViewHidden>
+            )
+        }}
+        leftOpenValue={80}
+        previewRowKey={"1"}
+        previewOpenValue={80}
+        previewOpenDelay={3000}
+        disableLeftSwipe={true}
+        showsVerticalScrollIndicator={false}
+        style={{
+            flex: 1, paddingBottom: 30, marginBottom: 40
+        }}
+    />
   )
 }
 
